de4e1169a3e00d6db5fd21bcdb8b1182c8f8de81
Jenna <jennabantjes@gmail.com>
2013-08-20 10:25:54 -0400

    Implement image rotator

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,40 +1,73 @@
 ;(function($){
   "use strict";
 
-    $('div.visit').addClass('hidden');
-    $('div.search').addClass('hidden');
-    $('div.exhibits').addClass('hidden');
-    $('div.tours').addClass('hidden');
+    $('div.visit').hide();
+    $('div.search').hide();
+    $('div.exhibits').hide();
+    $('div.tours').hide();
 
     $('a.visit').click(function(){
-        $('div.visit').slideToggle().toggleClass('hidden');
+        $('div.visit').slideToggle().show();
 
-        $('div.search').addClass('hidden');
-        $('div.exhibits').addClass('hidden');
-        $('div.tours').addClass('hidden');
+        $('div.search').hide();
+        $('div.exhibits').hide();
+        $('div.tours').hide();
     });
 
     $('a.search').click(function(){
-        $('div.search').slideToggle().toggleClass('hidden');
+        $('div.search').slideToggle().show();
 
-        $('div.visit').addClass('hidden');
-        $('div.exhibits').addClass('hidden');
-        $('div.tours').addClass('hidden');
+        $('div.visit').hide();
+        $('div.exhibits').hide();
+        $('div.tours').hide();
     });
 
     $('a.exhibits').click(function(){
-        $('div.exhibits').slideToggle().toggleClass('hidden');
+        $('div.exhibits').slideToggle().show();
 
-        $('div.search').addClass('hidden');
-        $('div.visit').addClass('hidden');
-        $('div.tours').addClass('hidden');
+        $('div.search').hide();
+        $('div.visit').hide();
+        $('div.tours').hide();
     });
 
     $('a.tours').click(function(){
-        $('div.tours').slideToggle().toggleClass('hidden');
+        $('div.tours').slideToggle().show();
 
-        $('div.search').addClass('hidden');
-        $('div.exhibits').addClass('hidden');
-        $('div.visit').addClass('hidden');
+        $('div.search').hide();
+        $('div.exhibits').hide();
+        $('div.visit').hide();
     });
+
+    var firstImage = $('#image-1');
+    var currentImage, nextImage;
+
+    setInterval(rotateImages, 4000);
+
+    function rotateImages() {
+        currentImage = $('li.current');
+        nextImage = $('li.next');
+
+        //the first image fades away
+        currentImage.animate({
+            opacity: 0
+        }, 4000, function () {
+            currentImage.removeClass('current');
+        });
+
+        //bring the second image into view
+        nextImage.animate({
+            opacity: 1
+        }, 4000, function () {
+
+            //make the new image the current image
+            nextImage.removeClass('next').addClass('current');
+            //the next image in the rotation becomes the 'next' image
+            nextImage.next().addClass('next').removeClass('behind');
+            //make the last image loop to the first image
+
+        });
+    }
+
 })(jQuery);
+
+
