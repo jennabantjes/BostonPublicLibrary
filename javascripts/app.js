0a2797b48aa55499196d18d4a0b7251085eedc99
Jenna Bantjes <jennabantjes@gmail.com>
2013-08-21 17:44:51 -0400

    Style typography

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,4 +1,42 @@
-;(function($){
+
+collapseNav();
+
+setInterval(rotateImages, 4000);
+
+function rotateImages() {
+    var image = $('.rotator li');
+    var firstImage = $('.rotator li').first;
+    var lastImage = $('.rotator li').last;
+    var currentImage, nextImage;
+
+    currentImage = $('li.current');
+    nextImage = $('li.next');
+
+    //the first image fades away
+    currentImage.animate({
+        opacity: 0
+    }, 4000, function () {
+        currentImage.removeClass('current');
+    });
+
+    //bring the second image into view
+    nextImage.animate({
+        opacity: 1
+    }, 4000, function () {
+
+        //make the new image the current image
+        nextImage.removeClass('next').addClass('current');
+        //the next image in the rotation becomes the 'next' image
+        nextImage.next().addClass('next');
+    });
+
+    //Loop back to beginning
+    if($(image[image.length - 2]).hasClass('current')){
+        $('#image-1').addClass('next');
+    }
+}
+
+function collapseNav(){
   "use strict";
 
     $('div.visit').hide();
@@ -37,37 +75,5 @@
         $('div.exhibits').hide();
         $('div.visit').hide();
     });
-
-    var firstImage = $('#image-1');
-    var currentImage, nextImage;
-
-    setInterval(rotateImages, 4000);
-
-    function rotateImages() {
-        currentImage = $('li.current');
-        nextImage = $('li.next');
-
-        //the first image fades away
-        currentImage.animate({
-            opacity: 0
-        }, 4000, function () {
-            currentImage.removeClass('current');
-        });
-
-        //bring the second image into view
-        nextImage.animate({
-            opacity: 1
-        }, 4000, function () {
-
-            //make the new image the current image
-            nextImage.removeClass('next').addClass('current');
-            //the next image in the rotation becomes the 'next' image
-            nextImage.next().addClass('next').removeClass('behind');
-            //make the last image loop to the first image
-
-        });
-    }
-
-})(jQuery);
-
+}
 
